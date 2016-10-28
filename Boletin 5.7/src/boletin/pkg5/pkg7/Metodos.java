1c4fe74922153594db6bd224189ba00432bd50e4
Diego <Diego@Diego>
2016-10-28 11:19:17 +0200

    final

--- Boletin 5.7/src/boletin/pkg5/pkg7/Metodos.java
+++ Boletin 5.7/src/boletin/pkg5/pkg7/Metodos.java
@@ -52,7 +52,7 @@ public class Metodos {
     }
     
     public void mostrarCirculo(){
-        JOptionPane.showMessageDialog(null, "El área del círculo es igual a " + (pi*(Math.pow(radio, 2))) + "cm^2");
+        JOptionPane.showMessageDialog(null, "El área del círculo es igual a " + (Pi*(Math.pow(radio, 2))) + "cm^2");
     }
     
 }
