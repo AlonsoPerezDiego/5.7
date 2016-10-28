c3ca90104423f58c00ff16533891d38173e363ca
Diego <Diego@Diego>
2016-10-28 12:00:19 +0200

    Fin del programa

--- Boletin 5.7/src/boletin/pkg5/pkg7/Boletin57.java
+++ Boletin 5.7/src/boletin/pkg5/pkg7/Boletin57.java
@@ -9,10 +9,10 @@ public class Boletin57 {
 
     public static void main(String[] args) {
         
+        int fallos = 0;
         int opciones = Integer.parseInt(JOptionPane.showInputDialog("Para calcular el área de un cuadrado teclea '1'. \n Para calcular el área de un triagulo teclea '2'. \n Y para calcular el área de un circulo teclea '3'."));
         
         Metodos area = new Metodos();
-        
         switch(opciones){
             
             case 1: area.setCuadrado();
@@ -27,7 +27,7 @@ public class Boletin57 {
             default:  JOptionPane.showMessageDialog(null, "Opción incorrecta");
         }
         
-        while(opciones<1|opciones>3){
+        while((opciones<1|opciones>3)&&fallos<=5){
            
             opciones = Integer.parseInt(JOptionPane.showInputDialog("Para calcular el área de un cuadrado teclea '1'. \n Para calcular el área de un triagulo teclea '2'. \n Y para calcular el área de un circulo teclea '3'. \n ¡No teclees otra cosa!"));
             
@@ -43,9 +43,18 @@ public class Boletin57 {
                         area.mostrarCirculo();
                         break;
                 default:  JOptionPane.showMessageDialog(null, "¡Opción incorrecta otra vez! \n ¡Introducelo bien de una vez!");
+                fallos = fallos + 1;
+                    System.out.println(fallos);
+
             }
         }
-            
+        
+        if(fallos!=6){
+        JOptionPane.showMessageDialog(null, "Adios.");
+        }
+        else{
+            JOptionPane.showMessageDialog(null, "¡Ya me cansé!\n¡Adios!");
+        }
     }
     
 }
